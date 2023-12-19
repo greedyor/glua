04e4037ac213069f26955f133032faf2099f3886
laijiefeng <laijiefeng1720@dingtalk.com>
2023-12-19 17:09:07 +0800

    add global_func.lua

---
+++ global_func.lua
@@ -0,0 +1,54 @@
+--
+-- Below is golang Implementation method，func For IDE use   
+--
+
+-- set return
+result = {
+    ---@param error error
+    errors = function(error)
+    end
+    ,
+    ---@param data any
+    success = function(data)
+    end
+}
+
+-- json func，encode and decode
+json = {
+    ---@param jsonStr string
+    ---@return string
+    decodeToTable = function(jsonStr)
+    end
+    ,
+    ---@param tableData table
+    ---@return string
+    encodeToTable = function(tableData)
+    end
+}
+
+-- request method func
+http = {
+    ---@param url string
+    ---@param method string
+    ---@param data any
+    ---@return string, error
+    request = function(url, method, data)
+    end
+    ,
+    ---@param url string
+    ---@return string, error
+    Get = function(url)
+    end
+    ,
+    ---@param url string
+    ---@param data table
+    ---@return string, error
+    PostForm = function(url, data)
+    end
+    ,
+    ---@param url string
+    ---@param data string
+    ---@return string, error
+    PostJSON = function(url, data)
+    end
+}
